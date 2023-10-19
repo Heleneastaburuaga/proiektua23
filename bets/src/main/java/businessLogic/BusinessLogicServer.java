6bd583eed3587011312f6546e64392bae77b4d07
Helene Astaburuaga <127048528+Heleneastaburuaga@users.noreply.github.com>
2023-10-19 13:05:23 +0200

    commit-11

--- bets/src/main/java/businessLogic/BusinessLogicServer.java
+++ bets/src/main/java/businessLogic/BusinessLogicServer.java
@@ -75,7 +75,7 @@ public class BusinessLogicServer extends JDialog {
 					public void actionPerformed(ActionEvent e) {
 						textArea.append("\n\n\nClosing the server... ");
 					    
-							//server.close();
+							
 						
 						System.exit(1);
 					}
